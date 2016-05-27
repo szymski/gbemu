3e10244fe1087073afd3543d76ee4fde938b1535
Szymon Jankowski <szymskipl@gmail.com>
2016-05-27 22:44:45 +0200

    More instructions

--- source/gbemu/cpu.d
+++ source/gbemu/cpu.d
@@ -81,14 +81,82 @@ class Cpu
 		registerInstruction!(0x27, "DAA", 0)(&daa);
 		registerInstruction!(0x28, "JR Z, 0x%X", 1)(&jr_z_n);
 		registerInstruction!(0x29, "ADD HL, HL", 0)(&add_reg_reg!("hl", "hl"));
-		registerInstruction!(0x2a, "LDI A, (HL)", 0)(&ldi_reg_regptr!("a", "hl"));
-		registerInstruction!(0x2b, "DEC HL", 0)(&dec_reg!"hl");
-		registerInstruction!(0x2c, "INC L", 0)(&inc_reg!"l");
-		registerInstruction!(0x2d, "DEC L", 0)(&dec_reg!"l");
-		registerInstruction!(0x2e, "LD L, 0x%X", 1)(&ld_reg_n!"l");
-		registerInstruction!(0x2f, "CPL", 0)(&cpl);
+		registerInstruction!(0x2A, "LDI A, (HL)", 0)(&ldi_reg_regptr!("a", "hl"));
+		registerInstruction!(0x2B, "DEC HL", 0)(&dec_reg!"hl");
+		registerInstruction!(0x2C, "INC L", 0)(&inc_reg!"l");
+		registerInstruction!(0x2D, "DEC L", 0)(&dec_reg!"l");
+		registerInstruction!(0x2E, "LD L, 0x%X", 1)(&ld_reg_n!"l");
+		registerInstruction!(0x2F, "CPL", 0)(&cpl);
 		registerInstruction!(0x30, "JR NC, 0x%X", 1)(&jr_nc_n);
+		registerInstruction!(0x31, "LD SP, 0x%X", 2)(&ld_reg_nn!"sp");
+		registerInstruction!(0x32, "LDD (HL), A", 0)(&ldd_regptr_reg!("hl", "a"));
+		registerInstruction!(0x33, "INC SP", 0)(&inc_reg!"sp");
+		registerInstruction!(0x34, "INC (HL)", 0)(&inc_regptr!"hl");
+		registerInstruction!(0x35, "DEC (HL)", 0)(&dec_regptr!"hl");
+		registerInstruction!(0x36, "LD (HL), 0x%X", 1)(&ld_regptr_n!"hl");
+		registerInstruction!(0x37, "SCF", 0)(&scf);
+		registerInstruction!(0x38, "JR C, 0x%X", 1)(&jr_c_n);
+		registerInstruction!(0x39, "ADD HL, SP", 0)(&add_reg_reg!("hl", "sp"));
+		registerInstruction!(0x3A, "LDD A, (HL)", 0)(&ldd_reg_regptr!("a", "hl"));
+		registerInstruction!(0x3B, "DEC SP", 0)(&dec_reg!"sp");
+		registerInstruction!(0x3C, "INC A", 0)(&inc_reg!"a");
+		registerInstruction!(0x3D, "DEC A", 0)(&dec_reg!"a");
+		registerInstruction!(0x3E, "LD A, 0x%X", 1)(&ld_reg_n!"a");
+		registerInstruction!(0x3F, "CCF", 0)(&ccf);
 
+		registerInstruction!(0x40, "LD B, B", 0)(&nop);
+		registerInstruction!(0x41, "LD B, C", 0)(&ld_reg_reg!("b", "c"));
+		registerInstruction!(0x42, "LD B, D", 0)(&ld_reg_reg!("b", "d"));
+		registerInstruction!(0x43, "LD B, E", 0)(&ld_reg_reg!("b", "e"));
+		registerInstruction!(0x44, "LD B, H", 0)(&ld_reg_reg!("b", "h"));
+		registerInstruction!(0x45, "LD B, L", 0)(&ld_reg_reg!("b", "l"));
+		registerInstruction!(0x46, "LD B, (HL)", 0)(&ld_reg_regptr!("b", "hl"));
+		registerInstruction!(0x47, "LD B, A", 0)(&ld_reg_reg!("b", "a"));
+
+		registerInstruction!(0x48, "LD C, B", 0)(&ld_reg_reg!("c", "b"));
+		registerInstruction!(0x49, "LD C, C", 0)(&nop);
+		registerInstruction!(0x4A, "LD C, D", 0)(&ld_reg_reg!("c", "d"));
+		registerInstruction!(0x4B, "LD C, E", 0)(&ld_reg_reg!("c", "e"));
+		registerInstruction!(0x4C, "LD C, H", 0)(&ld_reg_reg!("c", "h"));
+		registerInstruction!(0x4D, "LD C, L", 0)(&ld_reg_reg!("c", "l"));
+		registerInstruction!(0x4E, "LD C, (HL)", 0)(&ld_reg_regptr!("c", "hl"));
+		registerInstruction!(0x4F, "LD C, A", 0)(&ld_reg_reg!("c", "a"));
+
+		registerInstruction!(0x50, "LD D, B", 0)(&ld_reg_reg!("d", "b"));
+		registerInstruction!(0x51, "LD D, C", 0)(&ld_reg_reg!("d", "c"));
+		registerInstruction!(0x52, "LD D, D", 0)(&nop);
+		registerInstruction!(0x53, "LD D, E", 0)(&ld_reg_reg!("d", "e"));
+		registerInstruction!(0x54, "LD D, H", 0)(&ld_reg_reg!("d", "h"));
+		registerInstruction!(0x55, "LD D, L", 0)(&ld_reg_reg!("d", "l"));
+		registerInstruction!(0x56, "LD D, (HL)", 0)(&ld_reg_regptr!("d", "hl"));
+		registerInstruction!(0x57, "LD D, A", 0)(&ld_reg_reg!("d", "a"));
+
+		registerInstruction!(0x58, "LD E, B", 0)(&ld_reg_reg!("e", "b"));
+		registerInstruction!(0x59, "LD E, C", 0)(&ld_reg_reg!("e", "c"));
+		registerInstruction!(0x5A, "LD E, D", 0)(&ld_reg_reg!("e", "d"));
+		registerInstruction!(0x5B, "LD E, E", 0)(&nop);
+		registerInstruction!(0x5C, "LD E, H", 0)(&ld_reg_reg!("e", "h"));
+		registerInstruction!(0x5D, "LD E, L", 0)(&ld_reg_reg!("e", "l"));
+		registerInstruction!(0x5E, "LD E, (HL)", 0)(&ld_reg_regptr!("e", "hl"));
+		registerInstruction!(0x5F, "LD E, A", 0)(&ld_reg_reg!("e", "a"));
+
+		registerInstruction!(0x60, "LD H, B", 0)(&ld_reg_reg!("h", "b"));
+		registerInstruction!(0x61, "LD H, C", 0)(&ld_reg_reg!("h", "c"));
+		registerInstruction!(0x62, "LD H, D", 0)(&ld_reg_reg!("h", "d"));
+		registerInstruction!(0x63, "LD H, E", 0)(&ld_reg_reg!("h", "e"));
+		registerInstruction!(0x64, "LD H, H", 0)(&nop);
+		registerInstruction!(0x65, "LD H, L", 0)(&ld_reg_reg!("h", "l"));
+		registerInstruction!(0x66, "LD H, (HL)", 0)(&ld_reg_regptr!("h", "hl"));
+		registerInstruction!(0x67, "LD H, A", 0)(&ld_reg_reg!("h", "a"));
+
+		registerInstruction!(0x68, "LD L, B", 0)(&ld_reg_reg!("l", "b"));
+		registerInstruction!(0x69, "LD L, C", 0)(&ld_reg_reg!("l", "c"));
+		registerInstruction!(0x6A, "LD L, D", 0)(&ld_reg_reg!("l", "d"));
+		registerInstruction!(0x6B, "LD L, E", 0)(&ld_reg_reg!("l", "e"));
+		registerInstruction!(0x6C, "LD L, H", 0)(&ld_reg_reg!("l", "h"));
+		registerInstruction!(0x6D, "LD L, L", 0)(&nop);
+		registerInstruction!(0x6E, "LD L, (HL)", 0)(&ld_reg_regptr!("l", "hl"));
+		registerInstruction!(0x6F, "LD L, A", 0)(&ld_reg_reg!("l", "a"));
 
 		registerInstruction!(0xC3, "JP 0x%X", 2)(&jp_nn);
 	}
@@ -125,6 +193,12 @@ class Cpu
 		mixin(`memory[registers.` ~ register ~ `] = value;`);
 	}
 
+	// LD (reg), n
+	void ld_regptr_n(string register)(ubyte value) {
+		// TODO:
+		mixin(`memory[registers.` ~ register ~ `] = value;`);
+	}
+
 	// LD (reg), reg
 	void ld_regptr_reg(string register1, string register2)(ushort value) {
 		mixin(`memory[registers.` ~ register1 ~ `] = registers.` ~ register2 ~ `;`);
@@ -133,6 +207,11 @@ class Cpu
 	// LDI (reg), reg
 	void ldi_regptr_reg(string register1, string register2)(ushort value) {
 		mixin(`memory[registers.` ~ register1 ~ `++] = registers.` ~ register2 ~ `;`);
+	}
+
+	// LDD (reg), reg
+	void ldd_regptr_reg(string register1, string register2)(ushort value) {
+		mixin(`memory[registers.` ~ register1 ~ `--] = registers.` ~ register2 ~ `;`);
 	}
 
 	// LD reg, reg
@@ -148,6 +227,11 @@ class Cpu
 	// LDI reg, (reg)
 	void ldi_reg_regptr(string register1, string register2)() {
 		mixin(`registers.` ~ register1 ~ ` = memory[registers.` ~ register2 ~ `++];`);
+	}
+
+	// LDD reg, (reg)
+	void ldd_reg_regptr(string register1, string register2)() {
+		mixin(`registers.` ~ register1 ~ ` = memory[registers.` ~ register2 ~ `--];`);
 	}
 
 	// LD (nn), reg
@@ -171,6 +255,18 @@ class Cpu
 	void add_reg_reg(string register1, string register2)() {
 		// TODO: Flags
 		mixin(`registers.` ~ register1 ~ ` += registers.` ~ register2 ~ `;`);
+	}
+
+	// INC regptr
+	void inc_regptr(string register)() {
+		// TODO: Flags
+		mixin(`memory[registers.` ~ register ~ `] = cast(ubyte)(memory[registers.` ~ register ~ `] + 1);`);
+	}
+
+	// DEC regptr
+	void dec_regptr(string register)() {
+		// TODO: Flags
+		mixin(`memory[registers.` ~ register ~ `] = cast(ubyte)(memory[registers.` ~ register ~ `] - 1);`);
 	}
 
 	// RLCA
@@ -233,6 +329,11 @@ class Cpu
 		}
 	}
 
+	// JP nn
+	void jp_nn(ushort address) {
+		registers.pc = address;
+	}
+
 	// DAA
 	void daa() {
 		if(registers.flagNegative) {
@@ -262,9 +363,18 @@ class Cpu
 		registers.flagNegative = true;
 	}
 
-	// JP nn
-	void jp_nn(ushort address) {
-		registers.pc = address;
+	// SCF
+	void scf() {
+		registers.flagCarry = true;
+		registers.flagNegative = false;
+		registers.flagHalfCarry = false;
+	}
+
+	// CCF
+	void ccf() {
+		registers.flagCarry = !registers.flagCarry;
+		registers.flagHalfCarry = !registers.flagHalfCarry;
+		registers.flagNegative = false;
 	}
 
 	/*
@@ -275,7 +385,7 @@ class Cpu
 		auto opcode = memory[registers.pc];
 		registers.pc++;
 
-		log("Executing: 0x", opcode.to!string(16));
+		//log("Executing: 0x", opcode.to!string(16));
 
 		if(auto instruction = instructions[opcode]) {
 			logInstruction(instruction);
